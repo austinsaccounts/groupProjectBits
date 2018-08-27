9b5f5ba65d2fdfa3300b09b56976194ed1038374
Austin Cummings <Austins.accounts@gmail.com>
2018-08-26 22:38:46 -0700

    create initial && layout a prototype ui

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//begin business
+    alert("worked");
+//end business logic
+
+
+
+
+//begin user interface
+$(document).ready(function() {
+  alert("");
+  // event.prevent(Default);
+  //
+
+  $("img").click(function(){
+    alert("worked");
+      // var attkHealth =$("input[type=radio][name=size]:checked").attr('health');
+      // var attkdamage =$("input[type=radio][name=size]:checked").attr('health');
+      // var attkname =$("input[type=radio][name=size]:checked").attr('health');
+
+  })
+  // $("input[type=checkbox][name=toppings]").click(function(){
+    // var pizzapic = $(this).val();
+    // $('.'+pizzapic).toggleClass("hidden");
+
+
+
+})
